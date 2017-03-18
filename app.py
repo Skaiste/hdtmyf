2a126cdde91f2a3b12fedcf1e253129b1790384c
skaiste <butkute.skaiste@gmail.com>
2017-03-18 20:05:58 +0000

    json

--- app.py
+++ app.py
@@ -10,51 +10,51 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
+# when the endpoint is registered as a webhook, it must echo back
+# the 'hub.challenge' value it receives in the query arguments
+if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+	if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+		return "Verification token mismatch", 403
+	return request.args["hub.challenge"], 200
 
-    return "Hello world", 200
+return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
 def webhook():
 
-    # endpoint for processing incoming messaging events
+	# endpoint for processing incoming messaging events
 
-    data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+	data = request.get_json()
+	log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-    if data["object"] == "page":
+	if data["object"] == "page":
 
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
+		for entry in data["entry"]:
+			for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
+				if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+					sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+					recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+					message_text = messaging_event["message"]["text"]  # the message's text
 
 					with open('messages.json') as data_file:
 						data = json.load(data_file)
-					
+
 					log(data["messages"][0]["message"][0])
-                    send_message(sender_id, data["messages"][0]["message"][0])
+					send_message(sender_id, data["messages"][0]["message"][0])
 
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
+				if messaging_event.get("delivery"):  # delivery confirmation
+					pass
 
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
+				if messaging_event.get("optin"):  # optin confirmation
+					pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
+				if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+					pass
 
-    return "ok", 200
+	return "ok", 200
 
 
 def send_message(recipient_id, message_text):
